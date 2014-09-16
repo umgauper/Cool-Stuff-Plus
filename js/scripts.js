085d7330bc3edd4e415a13dde22e59c4150a6aed
Una <umgauper@gmail.com>
2014-09-15 22:50:17 -0700

    Added new items for sale, added jQuery button to toggle description of Hoshizaki ice maker, added Photoshopped banner image, added revised About page paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $('.bxslider').bxSlider();
+    $("button").click(
+        function() {
+            $(".viewMore").toggle("slow", function() {});
+        })
+
+
+
 });
