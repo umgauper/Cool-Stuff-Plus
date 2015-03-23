c0268263ba0c5440717ba4388e407b6c59a22986
Una Gauper <umgauper@gmail.com>
2015-03-23 18:10:17 +0700

    see previous commit message

---
+++ js/myApp.js
@@ -0,0 +1,11 @@
+//$('.bxslider').bxSlider(); this line fucks up the slider completely, images load though...
+var app = angular.module('myApp', [])
+
+    .controller('panelController',['$scope', function($scope) {
+        $scope.isActive = true;
+        $scope.setActive = function() {
+            $scope.isActive = !$scope.isActive;
+        };
+        $('.bxslider').bxSlider();
+    }]);
+
