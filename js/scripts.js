d84070e42bc73d25f95b1ebe6d06b7d4990b98aa
Una Gauper <umgauper@gmail.com>
2015-03-24 13:54:11 +0700

    removed angular code, added panel functionality with jQuery. fixed border-bottom not displaying in Chrome

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
+    $('.bxslider').bxSlider();
     $("button").click(
         function() {
             $(".viewMore").toggle("slow", function() {});
         });
+
+    //panel functions
+    $('div.other').hide();
+    $('li.refrig').on('click', function() {
+        $('li.refrig').css({"background-color": "#FFFFFF"});
+        $('li.other').css({"background-color": "#D3D3D3"});
+        $('div.other').hide();
+        $('div.refrig').show();
+    });
+    $('li.other').on('click', function() {
+        $('li.other').css({"background-color": "#FFFFFF"});
+        $('li.refrig').css({"background-color": "#D3D3D3"});
+        $('div.refrig').hide();
+        $('div.other').show();
+    });
+
 });
